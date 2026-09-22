2cc89800b72d1347f5c03740a64f00e8923b97f9
Fernando Torres <fernandoanselmot@gmail.com>
2026-09-22 13:35:45 -0400

    Created starter Angular files and completed the Assignment 1

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+type Named = { name: string };
+type Aged = { age: number };
+
+type Person = Named & Aged;
 
 @Component({
-  imports: [RouterOutlet],
   selector: 'app-root',
   styleUrl: './app.css',
   templateUrl: './app.html',
 })
 export class App {
   protected readonly title = signal('project1');
+  person1: Person = {
+    name: 'Fernando',
+    age: 28,
+  };
+
+  person2: Person = {
+    name: 'Juan',
+    age: 25,
+  };
 }
